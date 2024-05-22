08870a304fc48f426d6cdd3e482ebccb5bc221a9
BT_kung <u.nantanat@gmail.com>
2024-05-22 12:08:19 +0700

    tset

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,21 @@
 // login.js
-document.getElementById('login-form').addEventListener('submit', loginUser);
+document.getElementById("login-form").addEventListener("submit", loginUser);
 
 async function loginUser(e) {
   e.preventDefault();
-  const studentId = document.getElementById('student-id').value;
-  const password = document.getElementById('password').value;
-
+  const studentId = document.getElementById("student-id").value;
+  const password = document.getElementById("password").value;
+  //edfsgsfd
   try {
-    const userCredential = await auth.signInWithEmailAndPassword(`${studentId}@example.com`, password);
-    alert('Login successful!');
+    const userCredential = await auth.signInWithEmailAndPassword(
+      `${studentId}@example.com`,
+      password
+    );
+    alert("Login successful!");
     // Redirect to profile page
-    window.location.href = 'profile.html';
+    window.location.href = "profile.html";
   } catch (error) {
-    console.error('Error logging in:', error);
-    alert('Error: ' + error.message);
+    console.error("Error logging in:", error);
+    alert("Error: " + error.message);
   }
 }
